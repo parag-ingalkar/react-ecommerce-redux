a57a8b1b2e5e375a2ab34e20f3f9a94f220c3ac8
parag-ingalkar <paragingalkar@gmail.com>
2025-05-06 09:23:15 -0400

    Implemented shopping cacrt with Redux

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import './ProductList.css'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { addItemToCart } from './CartSlice';// Action to add product to cart
 
 const ProductList = () => {
+const dispatch = useDispatch();
+const cartItems = useSelector(state => state.cart.cartItems); // Get cart items globally
+
+const handleAddToCart = product => {
+    dispatch(addItemToCart(product));
+}
+
 
   const products = [
     { id: 1, name: 'Product A', price: 60 },
@@ -16,8 +25,11 @@ const ProductList = () => {
         {products.map(product => (
             <li key={product.id} className="product-list-item">
             <span>{product.name} - ${product.price}</span>
-            <button>
-                Add to Cart
+            <button
+                className={`add-to-cart-btn ${cartItems.some(item => item.id === product.id) ? 'disabled' : ''}`}
+                onClick={() => handleAddToCart(product)}
+                disabled = {cartItems.some(item => item.id === product.id)}>
+                {cartItems.some(item => item.id === product.id) ? 'Added' : 'Add to Cart'}
             </button>
             </li>
             ))}
